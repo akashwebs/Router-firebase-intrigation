f9928bd439ae9304600708fda925684ebad51e48
akashwebs <aksah.shil227@gmail.com>
2022-04-10 19:53:09 +0600

    react-router, firebasee, header and home routes setup

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAhnftFM4MqiCYMiRwf4Co_y326auVh0A",
+  authDomain: "router-firebase-5da5b.firebaseapp.com",
+  projectId: "router-firebase-5da5b",
+  storageBucket: "router-firebase-5da5b.appspot.com",
+  messagingSenderId: "850366151515",
+  appId: "1:850366151515:web:3e6248f61d10937fea787e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
